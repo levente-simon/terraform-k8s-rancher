1429081578a3ae7dcd7c38e5dd8b068748f8ccad
levente-simon <ls@leventesimon.com>
2022-02-20 19:09:30 +0100

    sensitive input vars

--- variables.tf
+++ variables.tf
@@ -5,9 +5,16 @@ variable "metallb_address_pool"   { type = string   }
 variable "proxy"                  { type = string   }
 variable "no_proxy"               { type = string   }
 variable "rancher_host"           { type = string   }
-variable "rancher_password"       { type = string   }
-variable "ssh_private_key"        { type = string   }
 variable "dns_server"             { type = string   }
 variable "dns_port"               { type = string   }
 variable "searchdomain"           { type = string   }
 
+variable "rancher_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "ssh_private_key" {
+  type      = string
+  sensitive = true
+}
