01c382f04253ba561ef89b3ac2273b9e2ad6259a
Levente Simon <ls@leventesimon.com>
2022-12-31 18:23:33 +0100

    addons removed, pki outsorced to cert-mgmt

--- variables.tf
+++ variables.tf
@@ -1,20 +1,12 @@
-variable "k8s_config_path"        { type = string   }
-variable "metallb_address_pool"   { type = string   }
-# variable "proxy"                  { type = string   }
-# variable "no_proxy"               { type = string   }
 variable "rancher_host"           { type = string   }
-variable "dns_server"             { type = string   }
-variable "dns_port"               { type = string   }
-variable "searchdomain"           { type = string   }
-variable "rancher_tls_crt"        { type = string   }
-variable "rancher_tls_key"        { type = string   }
+# variable "rancher_tls_crt"        { type = string   }
+# variable "rancher_tls_key"        { type = string   }
 variable "ca_crt"                 { type = string   }
 
 variable "rancher_password" {
   type      = string
   sensitive = true
 }
-
 
 variable "k8s_host" {
   type      = string
@@ -43,14 +35,4 @@ variable "k8s_cluster_client_token" {
   type      = string
   sensitive = true
   default   = ""
-}
-
-variable "longhorn_data_path" {
-  type    = string
-  default = "/data/longhorn"
-}
-
-variable "longhorn_default_replica_count" {
-  type    = number
-  default = 3
 }
