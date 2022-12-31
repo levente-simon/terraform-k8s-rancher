ede9bfcf1268d84fa60315601b9d670246dad27b
Levente Simon <ls@leventesimon.com>
2022-12-31 18:23:15 +0100

    addons removed, pki outsorced to cert-mgmt

--- rancher.tf
+++ rancher.tf
@@ -2,34 +2,16 @@ variable "module_depends_on" {
   type    = any
   default = []
 }
-resource "helm_release" "cert-manager" {
-  depends_on       = [ var.module_depends_on ]
-  name             = "cert-manager"
-
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  namespace        = "cert-manager"
-  create_namespace = true
-
-  set {
-    name  = "installCRDs"
-    value = "true"
-  }
-}
-
-resource "time_sleep" "wait_60_seconds_1" {
-  depends_on      = [ helm_release.cert-manager ]
-  create_duration = "60s"
-}
 
 resource "random_password" "bootstrap_password" {
+  depends_on       = [ var.module_depends_on ]
   length           = 16
   special          = true
   override_special = "_%@"
 }
 
 resource "kubernetes_namespace" "rancher" {
-  depends_on       = [ time_sleep.wait_60_seconds_1 ]
+  depends_on       = [ random_password.bootstrap_password ]
 
   lifecycle {
     ignore_changes  = all 
@@ -40,25 +22,25 @@ resource "kubernetes_namespace" "rancher" {
   }
 }
 
-resource "kubernetes_secret" "rancher_tls" {
-  depends_on = [ kubernetes_namespace.rancher ]
-
-  lifecycle {
-    ignore_changes  = all 
-  }
-
-  metadata {
-    name       = "tls-rancher-ingress"
-    namespace  = "cattle-system"
-  }
-
-  type       = "kubernetes.io/tls"
-  data       = {
-      "tls.crt" = var.rancher_tls_crt
-      "tls.key" = var.rancher_tls_key
-  }
-}
-
+# resource "kubernetes_secret" "rancher_tls" {
+#   depends_on = [ kubernetes_namespace.rancher ]
+# 
+#   lifecycle {
+#     ignore_changes  = all 
+#   }
+# 
+#   metadata {
+#     name       = "tls-rancher-ingress"
+#     namespace  = "cattle-system"
+#   }
+# 
+#   type       = "kubernetes.io/tls"
+#   data       = {
+#       "tls.crt" = var.rancher_tls_crt
+#       "tls.key" = var.rancher_tls_key
+#   }
+# }
+# 
 resource "kubernetes_secret" "tls_ca" {
   depends_on = [ kubernetes_namespace.rancher ]
 
